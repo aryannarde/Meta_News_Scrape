e96a4aaa9f7a5bca2291176360c6f3648a5e12d3
aryannarde <aryannarde1@gmail.com>
2024-12-10 23:26:25 +0530

    Task5.2

--- meta.py
+++ meta.py
@@ -1,21 +1,46 @@
 import requests as rq
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup
 import pandas as pd
-
 
 Metaurl = 'https://www.ft.com/stream/7e37c19e-8fa3-439f-a870-b33f0520bcc0'
 
 MetaHeder = {
-    'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0'
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0'
 }
 
-MetaResp = rq.get(url=Metaurl,headers=MetaHeder)
+MetaResp = rq.get(url=Metaurl, headers=MetaHeder)
+MetaSoup = BeautifulSoup(MetaResp.content, 'html.parser')
 
-MetaSoup = BeautifulSoup(MetaResp.content,'html.parser')
+dates = []
+headings = []
+contents = []
+images = []
 
-Meta_news = MetaSoup.find_all('li',attrs={'class':'o-teaser-collection__item o-grid-row'})
+DateNews = MetaSoup.select('div[class="o-teaser-collection o-teaser-collection--stream"] > ul > li')
 
-
+for n in DateNews:
+    date = n.select_one('div[class="stream-card__date"] > time')
+    dates.append(date.attrs['datetime'] if date else None)
     
-News_data = [news.text for news in Meta_news ]
-print(News_data)
+    heading = n.select_one('div[class="o-teaser__heading"] > a')
+    headings.append(heading.text.strip() if heading else None)
+    
+    content = n.select_one('p[class="o-teaser__standfirst"] > a')
+    contents.append(content.text.strip() if content else None)
+    
+    img = n.select_one('div[class="o-teaser__image-placeholder"] > img')
+    images.append(img.attrs['data-src'] if img else None)
+
+
+NewsData = {
+    'Date': dates,
+    'Heading': headings,
+    'Content': contents,
+    'Image': images
+}
+
+
+df = pd.DataFrame(NewsData)
+df.to_csv('news.csv')
+
+
